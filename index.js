4cb895d22b472958f8fc8d054651ab2f3f94af56
Jeremy Jongsma <jeremy@jongsma.org>
2016-02-25 12:39:05 -0600

    Changed behavior to just generate .ebextension config file to integrate better with common deployment

--- index.js
+++ index.js
@@ -1,82 +1,39 @@
 'use strict';
 
-var AWS = require('aws-sdk');
-var fs = require('fs');
-var Q = require('q');
-var _ = require('lodash');
+var Transform = require('stream').Transform;
 var gutil = require('gulp-util');
-var through = require('through2');
-var runInNewContext = require('vm').runInNewContext;
 
 module.exports = function(config) {
   
-  config = _.extend({
-    region: 'us-east-1'
-  }, config);
+  gutil.log('Creating EB environment config');
 
-  function configure(file, enc, callback) {
+  if (config) {
     
-    if (config.profile) {
-      AWS.config.credentials = new AWS.SharedIniFileCredentials({ profile: config.profile });
-      gutil.log('Using credentials from profile \'' + config.profile + '\'');
-    }
+    var options = [];
 
-    var iam = new AWS.IAM();
-
-    iam.getUser({}, function(err, data) {
-
-      if (err) {
-        callback(new gutil.PluginError('gulp-eb-config', err));
-      } else {
-
-        // Execute module to get settings variables
-        var module = { exports: {} };
-        if (file && file.contents) {
-          runInNewContext(file.contents, { module: module, exports: module.exports, require: require });
-        }
-
-        // Add hardcoded settings
-        if (config.settings) {
-          _.each(config.settings, function(v, k) {
-            module.exports[k] = v;
-          });
-        }
-
-        // Convert settings to EB format
-        var settings = _.keys(module.exports).map((k) => ({
+    for (var k in config) {
+      if (config.hasOwnProperty(k)) {
+        options.push({
           Namespace: 'aws:elasticbeanstalk:application:environment',
           OptionName: k,
-          Value: module.exports[k]
-        }));
-
-        var eb = new AWS.ElasticBeanstalk({ region: config.region });
-
-        gutil.log('Updating environment \'' + config.environment + '\' configuration with ' + settings.length + ' settings');
-        eb.updateEnvironment({
-          EnvironmentName: config.environment,
-          OptionSettings: settings
-        }, function (err, data) {
-          if (err) {
-            callback(new gutil.PluginError('gulp-eb-config', err));
-          } else {
-            gutil.log('Environment update running, please check AWS console for progress');
-            callback();
-          }
+          Value: config[k]
         });
-
       }
+    }
 
-    });
+    var stream = new Transform({ objectMode: true });
+    var contents = new Buffer(JSON.stringify({ option_settings: options }, null, 2));
+
+    // Convert settings to EB format
+    stream.push(new gutil.File({
+      cwd: './',
+      path: 'environmentvariables.config',
+      contents: contents
+    }));
 
   }
 
-  var piped = through.obj(configure);
-
-  piped.run = function(cb) {
-    configure(null, null, cb);
-  }
-
-  return piped;
+  return stream;
 
 }
 
