db63a6ad749784087619a57f3839949ed1606067
Jeremy Jongsma <jeremy@jongsma.org>
2016-02-25 12:48:41 -0600

    Revert "Changed behavior to just generate .ebextension config file to integrate better with common deployment"
    This reverts commit 4cb895d22b472958f8fc8d054651ab2f3f94af56.

--- index.js
+++ index.js
@@ -1,39 +1,82 @@
 'use strict';
 
-var Transform = require('stream').Transform;
+var AWS = require('aws-sdk');
+var fs = require('fs');
+var Q = require('q');
+var _ = require('lodash');
 var gutil = require('gulp-util');
+var through = require('through2');
+var runInNewContext = require('vm').runInNewContext;
 
 module.exports = function(config) {
   
-  gutil.log('Creating EB environment config');
+  config = _.extend({
+    region: 'us-east-1'
+  }, config);
 
-  if (config) {
+  function configure(file, enc, callback) {
     
-    var options = [];
-
-    for (var k in config) {
-      if (config.hasOwnProperty(k)) {
-        options.push({
-          Namespace: 'aws:elasticbeanstalk:application:environment',
-          OptionName: k,
-          Value: config[k]
-        });
-      }
+    if (config.profile) {
+      AWS.config.credentials = new AWS.SharedIniFileCredentials({ profile: config.profile });
+      gutil.log('Using credentials from profile \'' + config.profile + '\'');
     }
 
-    var stream = new Transform({ objectMode: true });
-    var contents = new Buffer(JSON.stringify({ option_settings: options }, null, 2));
+    var iam = new AWS.IAM();
 
-    // Convert settings to EB format
-    stream.push(new gutil.File({
-      cwd: './',
-      path: 'environmentvariables.config',
-      contents: contents
-    }));
+    iam.getUser({}, function(err, data) {
+
+      if (err) {
+        callback(new gutil.PluginError('gulp-eb-config', err));
+      } else {
+
+        // Execute module to get settings variables
+        var module = { exports: {} };
+        if (file && file.contents) {
+          runInNewContext(file.contents, { module: module, exports: module.exports, require: require });
+        }
+
+        // Add hardcoded settings
+        if (config.settings) {
+          _.each(config.settings, function(v, k) {
+            module.exports[k] = v;
+          });
+        }
+
+        // Convert settings to EB format
+        var settings = _.keys(module.exports).map((k) => ({
+          Namespace: 'aws:elasticbeanstalk:application:environment',
+          OptionName: k,
+          Value: module.exports[k]
+        }));
+
+        var eb = new AWS.ElasticBeanstalk({ region: config.region });
+
+        gutil.log('Updating environment \'' + config.environment + '\' configuration with ' + settings.length + ' settings');
+        eb.updateEnvironment({
+          EnvironmentName: config.environment,
+          OptionSettings: settings
+        }, function (err, data) {
+          if (err) {
+            callback(new gutil.PluginError('gulp-eb-config', err));
+          } else {
+            gutil.log('Environment update running, please check AWS console for progress');
+            callback();
+          }
+        });
+
+      }
+
+    });
 
   }
 
-  return stream;
+  var piped = through.obj(configure);
+
+  piped.run = function(cb) {
+    configure(null, null, cb);
+  }
+
+  return piped;
 
 }
 
